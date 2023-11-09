7a84a065e2be685f3d00b7e6ff192390a60368ab
Rojashree1807 <rojarojashree1807@gmail.com>
2023-11-09 14:54:16 +0530

    after created

---
+++ ConsAppAss4part2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsAppAss4part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First 10 Natural Numbers:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine("\n\nFirst 10 Even Natural Numbers:");
+            int evenCounter = 1;
+            while (evenCounter <= 20)
+            {
+                if (evenCounter % 2 == 0)
+                {
+                    Console.Write($"{evenCounter} ");
+                    evenCounter++;
+                }
+                else
+                {
+                    evenCounter++;
+                }
+            }
+
+            Console.WriteLine("\n\nFirst 10 Odd Natural Numbers:");
+            int oddCounter = 1;
+            do
+            {
+                if (oddCounter % 2 != 0)
+                {
+                    Console.Write($"{oddCounter} ");
+                }else
+                {
+                    oddCounter++;
+                }
+               
+            } while (oddCounter <= 20);
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
